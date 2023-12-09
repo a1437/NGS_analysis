099de269928b432a13fcd24743fd60e803ef57a2
a1437 <asifhossain38@yahoo.com>
2023-12-09 16:13:27 +0300

    Update diff.sh

--- differential_expression/diff.sh
+++ differential_expression/diff.sh
@@ -2,3 +2,6 @@ parallel -X -j8 "hisat2 -x drome_hisat_index -p 4 -U {} | samtools sort -o {/.}.
 featureCounts -a /biomsu/data/drome/ref/Drosophila_melanogaster.BDGP6.32.108.gtf -t gene -o counts.txt ./*bam
 /biomsu/data/scripts/run_edgeR_4vs4.R counts.txt deg_count.table
 cat deg_count.table | awk '{if($5 < 0.01) print $1}' > DEG.list
+
+
+spades.py
